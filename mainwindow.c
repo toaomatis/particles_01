c144c440853a2ed022cac8b3d0c404e981a94b8b
Mathijs de Kruyf <mathijs.de.kruyf@axon.tv>
2014-08-19 16:17:45 +0200

    Added axis for debugging and added keys for rotating X Y and Z axis

--- mainwindow.c
+++ mainwindow.c
@@ -24,18 +24,23 @@ const int WIN_WIDTH_I = 768;
 const int WIN_HEIGHT_I = 768;
 const float WIN_WIDTH_F = 768.0f;
 const float WIN_HEIGHT_F = 768.0f;
-const float WIN_DEPTH_F = 1.0f;
+const float WIN_DEPTH_F = 768.0f;
 
 float scale_width = 768.0f;
 float scale_height = 768.0f;
 float scale = 1.0f;
+/* X = 0, Y = 1, Z = 2 */
+int axis = 0;
+float theta[3] =
+{ 0.0f };
 
 static int init(int argc, char **argv);
 static void init_callbacks(void);
 static void render_scene_cb(void);
 static void displayReshape(int w, int h);
-
+static void special(int key, int x, int y);
 static void keyboard(unsigned char key, int x, int y);
+static void drawAxis();
 
 static struct Particle *particles;
 static int win_num = -1;
@@ -102,7 +107,7 @@ static void displayReshape(int w, int h)
     glLoadIdentity();
     //glOrtho(-1.0f * scale_width, scale_width, -1.0f * scale_height, scale_height, -10.0f, 10.0f);
     //glOrtho(-1.0f * scale_width, scale_width, -1.0f * scale_height, scale_height, -10.0f, 10.0f);
-    glOrtho(-1.0f * WIN_WIDTH_F, WIN_WIDTH_F, -1.0f * WIN_HEIGHT_F, WIN_HEIGHT_F, -10.0f, 10.0f);
+    glOrtho(-1.0f * WIN_WIDTH_F, WIN_WIDTH_F, -1.0f * WIN_HEIGHT_F, WIN_HEIGHT_F, -1.0f * WIN_DEPTH_F, WIN_DEPTH_F);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -113,6 +118,14 @@ static void animate()
     pthread_cond_signal(&(render_cond));
     pthread_cond_wait(&(paint_cond), &(paint_mutex));
 #endif
+    while (theta[axis] > 360.0f)
+    {
+        theta[axis] -= 360.0f;
+    };
+    while (theta[axis] < -360.0f)
+    {
+        theta[axis] += 360.0f;
+    };
     glutPostRedisplay();
 #if MUTEX_COND
     pthread_mutex_unlock(&(paint_mutex));
@@ -123,6 +136,7 @@ static void init_callbacks()
 {
     glutDisplayFunc(render_scene_cb);
     glutKeyboardFunc(keyboard);
+    glutSpecialFunc(special);
     glutIdleFunc(animate);
     glutReshapeFunc(displayReshape);
 }
@@ -163,10 +177,10 @@ static void keyboard(unsigned char key, int x, int y)
         }
             break;
 #if DEBUG_TIMING
-        case 'd':
-        {
-            debug = (debug == DEBUG_OFF ? DEBUG_ON : DEBUG_OFF);
-        }
+            case 'd':
+            {
+                debug = (debug == DEBUG_OFF ? DEBUG_ON : DEBUG_OFF);
+            }
             break;
 #endif
         case '-':
@@ -174,19 +188,52 @@ static void keyboard(unsigned char key, int x, int y)
             scale /= 2.0f;
         }
             break;
-        default:
         case '+':
         {
             scale *= 2.0f;
         }
             break;
-            {
+        default:
+        {
 
-            }
+        }
             break;
     }
     scale_width = WIN_WIDTH_F / scale;
     scale_height = WIN_HEIGHT_F / scale;
+}
+
+static void drawAxis()
+{
+    // save previous matrix
+    glPushMatrix();
+    // clear matrix
+    glLoadIdentity();
+    // apply rotations
+    glRotatef(theta[0], 1.0f, 0.0f, 0.0f);
+    glRotatef(theta[1], 0.0f, 1.0f, 0.0f);
+    glRotatef(theta[2], 0.0f, 0.0f, 1.0f);
+    // move the axes to the screen corner
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    /* Add Teapot for scale */
+    //glutWireTeapot(WIN_WIDTH_F / 2.0f);
+    // draw our axes
+    glBegin(GL_LINES);
+    // draw line for x axis
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(-1.0f * WIN_WIDTH_F, 0.0f, 0.0f);
+    glVertex3f(WIN_WIDTH_F, 0.0f, 0.0f);
+    // draw line for y axis
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -1.0f * WIN_HEIGHT_F, 0.0f);
+    glVertex3f(0.0f, WIN_HEIGHT_F, 0.0f);
+    // draw line for Z axis
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, -1.0f * WIN_DEPTH_F);
+    glVertex3f(0.0f, 0.0f, WIN_DEPTH_F);
+    glEnd();
+    // load the previous matrix
+    glPopMatrix();
 }
 
 static void render_scene_cb()
@@ -200,7 +247,11 @@ static void render_scene_cb()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     //gluLookAt(1.0,1.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0);
+    glRotatef(theta[0], 1.0f, 0.0f, 0.0f);
+    glRotatef(theta[1], 0.0f, 1.0f, 0.0f);
+    glRotatef(theta[2], 0.0f, 0.0f, 1.0f);
     glScalef(scale, scale, scale);
+    drawAxis();
     for (int i = 0; i < NUM_PARTICLES; i++)
     {
         glPushMatrix();
@@ -223,4 +274,45 @@ static void render_scene_cb()
         printf("render_scene_cb in %f secs (%f Hz) \n", secs_set, 1.0f / secs_set);
     }
 #endif
+}
+
+static void special(int key, int x, int y)
+{
+    switch (key)
+    {
+        /* Rotate Y axis */
+        case GLUT_KEY_LEFT:
+        {
+            theta[1] -= 10.0f;
+        }
+            break;
+        case GLUT_KEY_RIGHT:
+        {
+            theta[1] += 10.0f;
+        }
+            break;
+            /* Rotate X axis */
+        case GLUT_KEY_DOWN:
+        {
+            theta[0] -= 10.0f;
+        }
+            break;
+        case GLUT_KEY_UP:
+        {
+            theta[0] += 10.0f;
+        }
+            break;
+            /* Rotate Z axis. DAFUQ */
+        case GLUT_KEY_PAGE_DOWN:
+        {
+            theta[2] -= 10.0f;
+        }
+            break;
+        case GLUT_KEY_PAGE_UP:
+        {
+            theta[2] += 10.0f;
+        }
+            break;
+    }
+    fprintf(stdout, "rot %0.2f %0.2f %0.2f \n", theta[0], theta[1], theta[2]);
 }
