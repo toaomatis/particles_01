75665aa7e00224a12bfc55c3fecef751cc6bf86b
Mathijs de Kruyf <mathijs.de.kruyf@axon.tv>
2014-08-20 08:52:06 +0200

    Added http://www.plasmaphysics.org.uk/programs/coll3d_cpp.htm

---
+++ coll3d.h
@@ -0,0 +1,15 @@
+/*
+ * coll3d.h
+ *
+ *  Created on: Aug 20, 2014
+ *      Author: mathijs
+ */
+
+#ifndef COLL3D_H_
+#define COLL3D_H_
+
+#include "particle.h"
+
+void collision3D(struct Particle *p1, struct Particle *p2, int *error);
+
+#endif /* COLL3D_H_ */
